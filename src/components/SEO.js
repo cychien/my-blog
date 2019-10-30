c549ba8d8cfbb51c73bcdc38c13d4ffd3cd8dd22
cychien <xyz030206@gmail.com>
2019-10-30 10:46:26 +0800

    update seo

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -9,47 +9,52 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import { useStaticQuery, graphql } from 'gatsby'
 
-function SEO({ title, description, pathname, siteImage, isArticle }) {
-  const { site } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-            url
-            image
-          }
-        }
-      }
-    `
-  )
+function SEO({ title, description, image, pathname, isArticle }) {
+  const { site } = useStaticQuery(query)
+
+  const {
+    siteMetadata: { siteUrl, defaultTitle, defaultDescription, defaultImage },
+  } = site
+
+  const seo = {
+    title: title || defaultTitle,
+    description: description || defaultDescription,
+    image: `${siteUrl}${image || defaultImage}`,
+    url: `${siteUrl}${pathname || '/'}`,
+  }
 
   return (
-    <Helmet
-      title={title}
-      titleTemplate="%s | Justin Chien"
-      defaultTitle="Justin Chien's Blog"
-    >
-      <html lang="zh-tw" />
-      <meta name="description" content={description || site.description} />
-      <meta name="image" content={site.image} />
-      {site.url && (
-        <meta property="og:url" content={`${site.url}${pathname || '/'}`} />
-      )}
-      {isArticle && <meta property="og:type" content="article" />}
-      {site.title && <meta property="og:title" content={title || site.title} />}
-      {site.description && (
-        <meta
-          property="og:description"
-          content={description || site.description}
-        />
-      )}
-      {site.image && (
-        <meta property="og:image" content={siteImage || site.image} />
-      )}
-    </Helmet>
+    <>
+      <Helmet
+        title={seo.title}
+        titleTemplate="%s | Justin Chien"
+        defaultTitle="Justin Chien's Blog"
+      >
+        <meta name="description" content={seo.description} />
+        <meta name="image" content={seo.image} />
+        {seo.url && <meta property="og:url" content={seo.url} />}
+        {isArticle && <meta property="og:type" content="article" />}
+        {seo.title && <meta property="og:title" content={seo.title} />}
+        {seo.description && (
+          <meta property="og:description" content={seo.description} />
+        )}
+        {seo.image && <meta property="og:image" content={seo.image} />}
+      </Helmet>
+    </>
   )
 }
 
 export default SEO
+
+const query = graphql`
+  query SEO {
+    site {
+      siteMetadata {
+        defaultTitle: title
+        defaultDescription: description
+        siteUrl: url
+        defaultImage: image
+      }
+    }
+  }
+`
