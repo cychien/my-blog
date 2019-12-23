b08424ad9a5bedd524dfa19d90ecb72be52ec6c8
cychien <xyz030206@gmail.com>
2019-12-23 10:31:12 +0800

    publish: Dockerfile 中的 ENTRYPOINT

--- src/components/CodeBlock/CodeBlockView.js
+++ src/components/CodeBlock/CodeBlockView.js
@@ -1,23 +1,34 @@
 import React from 'react'
 import Highlight, { defaultProps } from 'prism-react-renderer'
 import theme from 'prism-react-renderer/themes/nightOwl'
+import cx from 'classnames'
 import './codeBlock.scss'
 
-function CodeBlockView({ children }) {
+function CodeBlockView({ children, className }) {
+  const language = className.replace(/language-/, '')
+
   return (
-    <Highlight {...defaultProps} theme={theme} code={children} language="css">
-      {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={className} style={{ ...style, padding: '20px' }}>
-          {tokens.map((line, i) => (
-            <div key={i} {...getLineProps({ line, key: i })}>
-              {line.map((token, key) => (
-                <span key={key} {...getTokenProps({ token, key })} />
-              ))}
-            </div>
-          ))}
-        </pre>
-      )}
-    </Highlight>
+    <div className="code-block">
+      <Highlight
+        {...defaultProps}
+        theme={theme}
+        code={children}
+        language={language}
+      >
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre className={cx(className, 'custom-pre')} style={{ ...style }}>
+            {tokens.map((line, i) => (
+              <div key={i} {...getLineProps({ line, key: i })}>
+                <span className="line-no">{i + 1}</span>
+                {line.map((token, key) => (
+                  <span key={key} {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            ))}
+          </pre>
+        )}
+      </Highlight>
+    </div>
   )
 }
 
